e7410ccb8cbf8df11d1347559011b78e65923a6f
Erling Tveter <erlingtv@stud.ntnu.no>
2019-03-12 21:39:47 +0100

    Added Makefile and main.c

---
+++ GNU make/GNU make/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GNU make
+//
+//  Created by Erling Tveter on 12/03/2019.
+//  Copyright © 2019 Erling Tveter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
